60c6899218947044bf168853b459a313ab4c6a99
MoisesVelez <moisesvelezfernandez@gmail.com>
2026-01-17 12:10:29 +0100

    treatment y surgery update

--- Entities/Surgery.cs
+++ Entities/Surgery.cs
@@ -2,8 +2,60 @@
 
 public class Surgery
 {
-    public string Type { get; set; }
-    public DateTime? Date { get; set; }
-    public string Procedure { get; set; }
-    public string PostOpCare { get; set; }
+    public string SurgeryName { get; set; }
+
+    public SurgeryType SurgeryType { get; set; }
+
+    public DateTime? SurgeryDate { get; set; }
+
+    public SurgeryOutcome Outcome { get; set; }
+
+    public List<SurgeryProcedure> Procedures { get; set; }
+
+    public string AnesthesiaType { get; set; }
+
+    public List<SurgeryMedication> PostOpMedications { get; set; }
+
+    public bool HospitalizationRequired { get; set; }
+
+    public int? HospitalizationDays { get; set; }
+
+    public bool FollowUpRequired { get; set; }
+
+    public DateTime? FollowUpDate { get; set; }
+}
+
+public enum SurgeryType
+{
+    Preventive,
+    Corrective,
+    Emergency,
+    Diagnostic,
+    Reconstructive
+}
+public enum SurgeryOutcome
+{
+    Successful,
+    SuccessfulWithComplications,
+    Failed,
+    InProgress
+}
+
+public class SurgeryProcedure
+{
+    public string name { get; set; }
+    public string description { get; set; }
+}
+
+public class SurgeryMedication
+{
+    public string Name { get; set; }
+
+    public string Dosage { get; set; }
+
+    public string Frequency { get; set; }
+
+    public int? DurationInDays { get; set; }
+
+    public string Notes { get; set; }
 }
--- Entities/Treatment.cs
+++ Entities/Treatment.cs
@@ -1,10 +1,31 @@
-﻿namespace SocioWeb.Entities;
-
-public class Treatment
+﻿namespace SocioWeb.Entities
 {
-    public string Medication { get; set; }
-    public string Dosage { get; set; }
-    public DateTime? StartDate { get; set; }
-    public DateTime? EndDate { get; set; }
-    public string Notes { get; set; }
+    public class Treatment
+    {
+        public string TreatmentName { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public string Instructions { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public List<TreatmentMedication> Medications { get; set; }
+
+        public bool FollowUpRequired { get; set; }
+    }
+
+    public class TreatmentMedication
+    {
+        public string Name { get; set; }
+
+        public string Dosage { get; set; }
+
+        public string Frequency { get; set; }
+
+        public int? DurationInDays { get; set; }
+
+        public string Notes { get; set; }
+
+    }
 }
