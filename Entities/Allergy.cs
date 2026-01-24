49b1028a63e2aa9532ed6f55731449c400a1304e
MoisesVelez <moisesvelezfernandez@gmail.com>
2026-01-24 14:34:51 +0100

    DV-205 Update

--- Entities/Allergy.cs
+++ Entities/Allergy.cs
@@ -1,5 +1,5 @@
 ﻿namespace SocioWeb.Entities;
-
+using System.ComponentModel.DataAnnotations;
 public class Allergy
 {
     public string Allergen { get; set; }
@@ -12,19 +12,38 @@ public class Allergy
     public string Notes { get; set; }
 }
 
+
+
 public enum AllergySeverity
 {
+    [Display(Name = "Leve")]
     Mild,
+
+    [Display(Name = "Moderada")]
     Moderate,
+
+    [Display(Name = "Severa")]
     Severe,
+
+    [Display(Name = "Anafilaxia")]
     Anaphylaxis
 }
 
 public enum AllergyType
 {
+    [Display(Name = "Alimentaria")]
     Food,
+
+    [Display(Name = "Medicamentos")]
     Medication,
+
+    [Display(Name = "Ambiental")]
     Environmental,
+
+    [Display(Name = "Parásitos")]
     Parasite,
+
+    [Display(Name = "Otra")]
     Other
 }
+
