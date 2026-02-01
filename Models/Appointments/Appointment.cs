afe55871235be323d8f7d86ac33593c99b502648
MoisesVelez <moisesvelezfernandez@gmail.com>
2026-02-01 18:00:53 +0100

    CItas I

--- Models/Appointments/Appointment.cs
+++ Models/Appointments/Appointment.cs
@@ -8,4 +8,8 @@ public class Appointment
     public string Id { get; set; }
     public DateTime Date { get; set; }
     public string? Reason { get; set; }
+    public string PetName { get; set; }
+    public string OwnerName { get; set; }
+    public string Phone { get; set; }
+    public string Status { get; set; }
 }
