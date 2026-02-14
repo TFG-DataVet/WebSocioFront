7947c5383e76e87a296df55792a2cb9834b356d9
MoisesVelez <moisesvelezfernandez@gmail.com>
2026-02-14 18:54:31 +0100

    Borrar y crear citas

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SocioWeb;
+using SocioWeb.Services.AppointmentService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<AppointmentService>();
 
 await builder.Build().RunAsync();
---
+++ Services/AppointmentService/AppointmentService.cs
@@ -0,0 +1,44 @@
+﻿namespace SocioWeb.Services.AppointmentService;
+
+using System;
+using System.Collections.Generic;
+
+public class AppointmentService
+{
+    public List<Appointment> Appointments { get; } = new List<Appointment>();
+
+    public AppointmentService()
+    {
+        // Citas de ejemplo
+        Appointments.Add(new Appointment
+        {
+            Id = "1",
+            PetName = "Firulais",
+            OwnerName = "Juan Pérez",
+            Phone = "555-1234",
+            Start = DateTime.Today.AddHours(10),
+            End = DateTime.Today.AddHours(11),
+            Reason = "Vacunación",
+            Status = "Pendiente"
+        });
+
+        Appointments.Add(new Appointment
+        {
+            Id = "2",
+            PetName = "Michi",
+            OwnerName = "Ana López",
+            Phone = "555-9876",
+            Start = DateTime.Today.AddDays(1).AddHours(16),
+            End = DateTime.Today.AddDays(1).AddHours(17),
+            Reason = "Control general",
+            Status = "Confirmada"
+        });
+    }
+
+    public void Add(Appointment appointment)
+    {
+        Appointments.Add(appointment);
+    }
+}
+
+
